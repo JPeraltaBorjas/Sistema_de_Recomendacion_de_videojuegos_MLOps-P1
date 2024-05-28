eba8484bb3aff1eef001647f3d6277c391284c37
Jay <jmw.peralta@gmail.com>
2024-05-29 01:31:51 +0200

    act_6

--- README.md
+++ README.md
@@ -1,2 +1,91 @@
-# MLOps-P1
-Proyecto Machine Learning Operations (MLOps) by Jay Peralta Borjas
+# MLOps-P1: Proyecto Machine Learning Operations
+
+Descripción breve y concisa de lo que hace tu proyecto.
+
+## Tabla de Contenidos
+
+- [Introducción](#introducción)
+- [Instalación](#instalación)
+- [Uso](#uso)
+- [Características](#características)
+- [Contribución](#contribución)
+- [Pruebas](#pruebas)
+- [Contacto](#contacto)
+
+## Introducción
+
+Una descripción más detallada sobre tu proyecto, sus objetivos y el problema que soluciona. Aquí también puedes mencionar las tecnologías usadas y cualquier contexto relevante.
+
+## Instalación
+
+Instrucciones paso a paso para instalar el entorno y ejecutar el proyecto localmente.
+
+```bash
+# Clona el repositorio
+git clone https://github.com/tu_usuario/tu_proyecto.git
+
+# Navega al directorio del proyecto
+cd tu_proyecto
+
+# Crea y activa un entorno virtual (opcional pero recomendado)
+python -m venv venv
+# En Windows
+.\venv\Scripts\activate
+# En macOS y Linux
+source venv/bin/activate
+
+# Instala las dependencias
+pip install -r requirements.txt
+
+## Uso
+
+Instrucciones y ejemplos sobre cómo usar el proyecto. Puedes incluir fragmentos de código, comandos de consola y capturas de pantalla si es necesario.
+
+```bash
+# Comando para ejecutar el proyecto
+python main.py
+```
+
+## Características
+
+Lista de las características más importantes de tu proyecto.
+
+- Característica 1
+- Característica 2
+- Característica 3
+
+## Contribución
+
+Instrucciones para que otros puedan contribuir a tu proyecto. Esto puede incluir cómo clonar el repositorio, crear ramas, realizar pull requests y seguir las convenciones de codificación.
+
+```bash
+# Clona el repositorio
+git clone https://github.com/tu_usuario/tu_proyecto.git
+
+# Crea una rama nueva para tu característica o arreglo
+git checkout -b nombre-de-tu-rama
+
+# Realiza tus cambios y haz commits
+git commit -m "Descripción de tus cambios"
+
+# Empuja tu rama al repositorio remoto
+git push origin nombre-de-tu-rama
+
+# Crea un pull request desde GitHub
+```
+
+## Pruebas
+
+Instrucciones sobre cómo ejecutar las pruebas para tu proyecto. Incluye ejemplos de cómo ejecutar pruebas unitarias y de integración.
+
+```bash
+# Ejecutar las pruebas
+pytest
+```
+
+
+## Contacto
+
+Jay Peralta Borjas - [jmw.peralta@gmail.com](mailto:jmw.peralta@gmail.com)
+
+Link al proyecto en GitHub: https://github.com/JPeraltaBorjas/MLOps-P1.git
--- main.py
+++ main.py
@@ -11,6 +11,9 @@ app = FastAPI(title='Proyecto Machine Learning Operations (MLOps) by Jay Peralta
 
 # Cargamos los dataset
 df_developer= pd.read_parquet('Dataset/df1_developer.parquet')
+df_userdata=pd.read_parquet('Dataset/df2_userdata.parquet')
+df_UserForGenre_1=pd.read_parquet('Dataset/df3_UserForGenre_1.parquet')
+df_UserForGenre_2=pd.read_parquet('Dataset/df3_UserForGenre_2.parquet')
 
 
 # Iniciamos la API
@@ -20,13 +23,15 @@ async def index():
 
 
 
-# Función informe historico por desarrollador
-@app.get('/Desarrollador/')
+# Función 1
+@app.get('/desarrollador/')
 def developer(desarrollador:str):
 
-    """Se ingresa el nombre del desarrollador
+    """
+    Se ingresa el nombre del desarrollador
     Devuelve como resultado un resumen de la cantidad de lanzamientos 
-    y contenido Free to play por año"""
+    y contenido Free to play por año
+    """
 
     # obtenemos el dataframe segun el desarrollador
     df_developer_filtrado= df_developer[df_developer['developer'] == desarrollador]
@@ -55,4 +60,68 @@ def developer(desarrollador:str):
         "Info": resultado.to_dict(orient='records')
     }
     
+    return res
+
+
+
+
+# Función 2
+@app.get('/userdata/')
+def userdata(User_id:str):
+
+    """
+    Recibe como parametro el id del usuario
+    Debe devolver `cantidad` de dinero gastado por el usuario, 
+    el `porcentaje` de recomendación en base a reviews.recommend y `cantidad de items`.
+    """
+
+    condicion=(df_userdata['user_id'] == User_id)
+    usuario=df_userdata[condicion]
+
+    num_usuario='Usuario '+str(usuario.index[0] + 1)
+
+    dinero_gastado=int(usuario.price.iloc[0])
+    dinero_gastado=str(dinero_gastado) + ' USD'
+
+    recomendacion=str(int(usuario.proportion.iloc[0])) + '%'
+
+    cant_items=usuario.item_id.iloc[0]
+
+    res={
+        'Usuario '+str(usuario.index[0] + 1): User_id,
+        "Dinero gastado": dinero_gastado,
+        "porcentaje de recomendación": recomendacion,
+        "cantidad de items": cant_items
+    }
+    return res
+
+
+
+# Función 3
+@app.get('/UserForGenre/')
+def UserForGenre(genero:str):
+
+    """ 
+    Recibe como parametro de entrada el genero del juego
+    devuelve el usuario que acumula más horas jugadas para el género dado 
+    y una lista de la acumulación de horas jugadas por año de lanzamiento.
+    """
+
+    condicion_1= (df_UserForGenre_1['genres'] == genero)
+    seleccion=df_UserForGenre_1[condicion_1]
+    usuario=seleccion.iloc[0,1]
+    tiempo_total=seleccion.iloc[0,2]
+
+    condicion_2=(df_UserForGenre_2['genres'] == genero)&(df_UserForGenre_2['user_id'] == usuario)
+    tiempo_anio=df_UserForGenre_2[condicion_2]
+    tiempo_anio=tiempo_anio.loc[:,['year','playtime_forever']].sort_values('year',ascending=False)
+    tiempo_anio.columns=['Año','Horas']
+
+    anios_dict = tiempo_anio.to_dict(orient='records')
+
+    res={
+        'Usuario con más horas jugadas para el Género ' + genero: usuario,
+        'Hojas jugadas': anios_dict
+    }
+
     return res
